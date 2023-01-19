b3a1a2245cbc6994be48532c572486d018db5b7e
Alipebt <1160756683@qq.com>
2023-01-19 19:45:06 +0800

    服务端实现

--- README.md
+++ README.md
@@ -15,5 +15,61 @@
 var _ Codec = (*GobCodec)(nil)
 ```
 
+### 1.2 通信过程
 
+客户端与服务端通信一般需要协商一些内容，例如 HTTP 报文，分为 `header` 和 `body` 两部分，`body` 的格式和长度通过 `header` 中的 `Content-Type` 和 `Content-Length` 指定，服务端通过解析 `header` 就能够知道如何从 `body` 中读取需要的信息。对于 `RPC` 协议来说，这部分协商是需要自主设计的。一般在报文的最开始会规划固定的字节，来协商相关的信息。比如第1个字节用来表示序列化方式，第2个字节表示压缩方式，第3-6字节表示 header 的长度，7-10 字节表示 body 的长度。
 
+```
+| Option{MagicNumber: xxx, CodecType: xxx} | Header{ServiceMethod ...} | Body interface{} |
+| <------      固定 JSON 编码      ------> | <-------   编码方式由 CodeType 决定  ------->|
+```
+
+在一次连接中，Option 固定在报文的最开始，Header 和 Body 可以有多个，即报文可能是这样的。
+
+```
+| Option | Header1 | Body1 | Header2 | Body2 | ...
+```
+
+### 1.3 服务端的实现
+
+首先需要一个`server`结构体，不需要任何成员字段，通过`NewServer()`创建一个默认`server`实例。
+
+`Accept()`是`server`的一个方法，需要`net.Listener`作为参数。内部通过`for`循环等待`socket`建立连接，并开启协程处理，处理过程交给了`ServerConn()`方法
+
+启动方法很简单：
+
+```go
+lis,_ := net.Listen("tcp",":9999")
+geerpc.Accept(lis)
+```
+
+`ServeConn` 的实现就和之前讨论的通信过程紧密相关了，首先使用 `json.NewDecoder` 反序列化得到 Option 实例，检查 `MagicNumber` 和` CodeType `的值是否正确。然后根据`CodeType `得到对应的消息编解码器，接下来的处理交给 `serverCodec`。
+
+`serveCodec` 的过程非常简单。主要包含三个阶段
+
+- 读取请求 `readRequest`
+- 处理请求 `handleRequest`
+- 回复请求 `sendResponse`
+
+在一次连接中，允许接收多个请求，即多个` request header `和 `request body`，因此这里使用了 `for` 无限制地等待请求的到来，直到发生错误（例如连接被关闭，接收到的报文有问题等），这里需要注意的点有三个：
+
+- `handleRequest` 使用了协程并发执行请求。
+- 处理请求是并发的，但是回复请求的报文必须是逐个发送的，并发容易导致多个回复报文交织在一起，客户端无法解析。在这里使用锁(`sending`)保证。
+
+`New`接口：
+
+```go
+func New(typ Type ) value
+```
+
+`New` 返回一个 `Value`，表示指向指定类型的新零值的指针。也就是说，返回值的类型是 `PointerTo(typ)`。
+
+`func (Value) Inetrface` 接口：
+
+接口将` v` 的当前值作为接口{}返回。它相当于：
+
+```
+var i interface{} = (v 的基础值)
+```
+
+如果 `Value` 是通过访问未导出的结构字段获得的，它会发生恐慌。
--- RPCserver/server.go
+++ RPCserver/server.go
@@ -1,8 +1,156 @@
-package RPCserver
+package geerpc
 
-import "RPC/codec"
+import (
+	"RPC/codec"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"reflect"
+	"sync"
+)
+
+const MagicNumber = 0x3bef5d
 
 type Option struct {
-	length uint64
-	header codec.Type
+	MagicNumber int        //掩码
+	CodecType   codec.Type //编解码的类型
+}
+
+var DefultOption = &Option{
+	MagicNumber: MagicNumber,
+	CodecType:   codec.GobType,
+}
+
+// 定义空结构体
+type Server struct{}
+
+// 通过该函数来构造一个新server
+func NewServer() *Server {
+	return new(Server)
+}
+
+// 作为*Server负责调用server的方法
+var DefultServer = NewServer()
+
+// server的方法
+func (server *Server) Accept(lis net.Listener) {
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Panicln("rpc server: accept error: ", err)
+			return
+		}
+		go server.ServerConn(conn)
+	}
+}
+
+// 用户调用的Accept
+func Accept(lis net.Listener) {
+	//server := new(Server)
+	//server.Accept(lis)
+	DefultServer.Accept(lis)
+
+}
+
+func (server *Server) ServerConn(conn io.ReadWriteCloser) {
+	defer func() {
+		_ = conn.Close()
+	}()
+
+	var opt Option
+
+	if err := json.NewDecoder(conn).Decode(&opt); err != nil {
+		log.Println("rpc server: option error: ", err)
+		return
+	}
+
+	if opt.MagicNumber != MagicNumber {
+		log.Printf("rpc server: invalid magic number %x", opt.MagicNumber)
+		return
+	}
+
+	f := codec.NewCodecFuncMap[opt.CodecType]
+
+	if f == nil {
+		log.Printf("rpc server: invalid codec type %s", opt.CodecType)
+		return
+	}
+
+	server.serverCodec(f(conn))
+
+}
+
+var invalidRequest = struct{}{}
+
+func (server *Server) serverCodec(cc codec.Codec) {
+
+	sending := new(sync.Mutex) // make sure to send a complete response
+	wg := new(sync.WaitGroup)  // wait until all request are handled
+
+	for {
+		req, err := server.readRequest(cc)
+		if err != nil {
+			if req == nil {
+				break
+			}
+			req.h.Error = err.Error()
+			server.sendResponse(cc, req.h, invalidRequest, sending)
+			continue
+		}
+		wg.Add(1)
+		go server.handleRequest(cc, req, sending, wg)
+	}
+	wg.Wait()
+	_ = cc.Close()
+}
+
+type request struct {
+	h            *codec.Header
+	argv, replyv reflect.Value
+}
+
+func (server *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
+	var h codec.Header
+	if err := cc.ReadHeader(&h); err != nil {
+		if err != io.EOF && err != io.ErrUnexpectedEOF {
+			log.Println("rpc server: read header error: ", err)
+		}
+		return nil, err
+	}
+	return &h, nil
+}
+
+func (server *Server) readRequest(cc codec.Codec) (*request, error) {
+
+	h, err := server.readRequestHeader(cc)
+	if err != nil {
+		return nil, err
+	}
+	req := &request{h: h}
+
+	req.argv = reflect.New(reflect.TypeOf("")) //返回指向“”类型的新指针
+
+	if err = cc.ReadBody(req.argv.Interface()); err != nil {
+		//Interface()返回当前的值为Interface{}
+		log.Panicln("rpc server: read argv error: ", err)
+	}
+
+	return req, nil
+}
+
+func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interface{}, sending *sync.Mutex) {
+	sending.Lock()
+	defer sending.Unlock()
+	if err := cc.Write(h, body); err != nil {
+		log.Println("rpc server: write response error: ", err)
+	}
+}
+
+func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
+	defer wg.Done()
+	log.Println(req.h, req.argv.Elem())
+	req.replyv = reflect.ValueOf(fmt.Sprintf("rpc resp %d", req.h.Seq))
+	server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
 }
